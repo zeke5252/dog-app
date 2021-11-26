29121883f26373025f60f3394dda4934c5339e6f
Zeke Lin <zeke5252@yahoo.com.tw>
2021-11-26 14:46:27 +0800

    51178a3

---
+++ _nuxt/static/1637909149/dogs/232582/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdog-app\u002F_nuxt\u002Fstatic\u002F1637909149",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdogs\u002F232582",config:{_app:{basePath:"\u002Fdog-app\u002F",assetsPath:"\u002Fdog-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
